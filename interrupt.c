a056905ac302c50ac53ccd653913253e68fe55f9
mrm <kei.id0305@gmail.com>
2021-03-12 14:21:50 +0900

    update vectortable

--- interrupt.c
+++ interrupt.c
@@ -1,9 +1,14 @@
 #include "arm.h"
 #include "uni.h"
 #include "interrupt.h"
+#include "printf.h"
 
 extern u64 vectortable;
 
 void intr_init() {
   set_vbar_el1(vectortable);
+}
+
+void sync_handler() {
+  printf("sync\n");
 }
