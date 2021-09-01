f41bcf48be9902d2fe512de3c33b84cefc016f45
mrm <kei.id0305@gmail.com>
2021-09-02 04:14:16 +0900

    va worked?

--- include/memmap.h
+++ include/memmap.h
@@ -10,13 +10,10 @@
 //#define PERIPHERAL_BASE 0xfe000000
 //#define PERIPHERAL_BASE 0x3f000000
 
-//#define VA_START    0xffffff8000000000
-#define VA_START  0
+#define VA_START    0xffffff8000000000
 
-//#define V2P(v)  ((v) - VA_START)
-//#define P2V(p)  ((p) + VA_START)
-#define V2P(v)  v
-#define P2V(p)  p
+#define V2P(v)  ((v) - VA_START)
+#define P2V(p)  ((p) + VA_START)
 
 #define PKERNBASE   0x40000000
 #define KERNBASE    (PKERNBASE + VA_START)
