d4e82f31b03b65389f8c8fcfeab606c2d0e5f451
mrm <cmpl.error@gmail.com>
2021-09-15 10:45:16 +0900

    update test

--- usr/test.c
+++ usr/test.c
@@ -3,15 +3,15 @@
 
 /* test */
 void proc1() {
-  u64 s = 0;
+  char *s = "proc1\n";
   for(;;) {
-    write("proc1\n", 6);
+    write(s, strlen(s));
   }
 }
 
 void proc2() {
-  u64 s = 0;
+  char *s = "proc2\n";
   for(;;) {
-    write("proc2\n", 6);
+    write(s, strlen(s));
   }
 }
