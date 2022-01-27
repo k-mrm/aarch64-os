e40e2b10eae8cf2ea88e2064be6802a5cb1962c7
mrm <cmpl.error@gmail.com>
2022-01-27 12:39:53 +0900

    gicv3

--- kernel/driver/gicv3.c
+++ kernel/driver/gicv3.c
@@ -25,11 +25,17 @@
 #define GICC_AEOIR  (0x24)
 
 #define GICR_CTLR           (0x0)
-#define GICR_ISENABLER0     (0x100)
-#define GICR_ICENABLER0     (0x180)
-#define GICR_IPRIORITYR(n)  (0x400+(n)*4)
-#define GICR_ICFGR0(n)      (0xc00)
-#define GICR_ICFGR1(n)      (0xc04)
+#define GICR_WAKER          (0x14)
+
+#define SGI_BASE  0x10000
+#define GICR_IGROUPR0       (SGI_BASE+0x80)
+#define GICR_ISENABLER0     (SGI_BASE+0x100)
+#define GICR_ICENABLER0     (SGI_BASE+0x180)
+#define GICR_ICPENDR0       (SGI_BASE+0x280)
+#define GICR_IPRIORITYR(n)  (SGI_BASE+0x400+(n)*4)
+#define GICR_ICFGR0         (SGI_BASE+0xc00)
+#define GICR_ICFGR1         (SGI_BASE+0xc04)
+#define GICR_IGRPMODR0      (SGI_BASE+0xd00)
 
 static bool is_sgi_ppi(u32 id);
 
@@ -63,6 +69,16 @@ static inline void w_icc_eoir1_el1(u32 x) {
   asm volatile("msr S3_0_C12_C12_1, %0" : : "r"(x));
 }
 
+static inline u32 icc_sre_el1() {
+  u32 x;
+  asm volatile("mrs %0, S3_0_C12_C12_5" : "=r"(x));
+  return x;
+}
+
+static inline void w_icc_sre_el1(u32 x) {
+  asm volatile("msr S3_0_C12_C12_5, %0" : : "r"(x));
+}
+
 static struct {
   char *gicd;
   char *rdist_addrs[NCPU];
@@ -90,10 +106,28 @@ static void gic_enable_int(u32 intid) {
   wd32(GICD_ISENABLER(intid / 32), is);
 }
 
+static void gicr_enable_int(u32 cpuid, u32 intid) {
+  if(!is_sgi_ppi(intid))
+    panic("gicr_enable_int");
+
+  u32 is = rr32(cpuid, GICR_ISENABLER0);
+  is |= 1 << (intid % 32);
+  wr32(cpuid, GICR_ISENABLER0, is);
+}
+
 static void gic_clear_pending(u32 intid) {
   u32 ic = rd32(GICD_ICPENDR(intid / 32));
   ic |= 1 << (intid % 32);
   wd32(GICD_ICPENDR(intid / 32), ic);
+}
+
+static void gicr_clear_pending(u32 cpuid, u32 intid) {
+  if(!is_sgi_ppi(intid))
+    panic("gicr_clear_pending");
+
+  u32 ic = rr32(cpuid, GICR_ICPENDR0);
+  ic |= 1 << (intid % 32);
+  wr32(cpuid, GICR_ICPENDR0, ic);
 }
 
 static void gic_set_prio(u32 intid, u32 prio) {
@@ -102,16 +136,32 @@ static void gic_set_prio(u32 intid, u32 prio) {
   wd32(GICD_IPRIORITYR(intid / 4), p);
 }
 
+static void gicr_set_prio(u32 cpuid, u32 intid, u32 prio) {
+  if(!is_sgi_ppi(intid))
+    panic("gicr_set_prio");
+
+  u32 p = rr32(cpuid, GICR_IPRIORITYR(intid / 4));
+  p &= ~((u32)0xff << (intid % 4 * 8)); // set prio 0
+  wr32(cpuid, GICR_IPRIORITYR(intid / 4), p);
+}
+
 static void gic_set_target(u32 intid, u32 cpuid) {
   u32 itargetsr = rd32(GICD_ITARGETSR(intid / 4));
   itargetsr &= ~((u32)0xff << (intid % 4 * 8));
   wd32(GICD_ITARGETSR(intid / 4), itargetsr | ((u32)(1 << cpuid) << (intid % 4 * 8)));
 }
 
-void gic_setup_ppi(u32 intid, int prio) {
-  gic_set_prio(intid, prio);
-  gic_clear_pending(intid);
-  gic_enable_int(intid);
+static void gicr_wait_rwp(u32 cpuid) {
+  u32 ctlr = rr32(cpuid, GICR_CTLR);
+  while((ctlr >> 3) & 1)  // RWP
+    ;
+}
+
+void gic_setup_ppi(u32 cpu, u32 intid, int prio) {
+  printk("setup ppi %d\n", cpu);
+  gicr_set_prio(cpu, intid, prio);
+  gicr_clear_pending(cpu, intid);
+  gicr_enable_int(cpu, intid);
 }
 
 void gic_setup_spi(u32 intid, int prio) {
@@ -123,39 +173,62 @@ void gic_setup_spi(u32 intid, int prio) {
 
 static void gic_cpu_init() {
   w_icc_igrpen1_el1(0);
+
   w_icc_pmr_el1(0xff);
 }
 
-static void gicd_init() {
+static void gic_dist_init() {
   wd32(GICD_CTLR, 0);
 
   for(int i = 0; i < 32; i++)
     wd32(GICD_IGROUPR(i), ~0);
 }
 
+static void gic_redist_init(u32 cpuid) {
+  wr32(cpuid, GICR_CTLR, 0);
+
+  w_icc_sre_el1(icc_sre_el1() | 1);
+
+  /* Non-secure Group1 */
+  wr32(cpuid, GICR_IGROUPR0, ~0);
+  wr32(cpuid, GICR_IGRPMODR0, 0);
+
+  wr32(cpuid, GICR_ICFGR1, 0);
+
+  /* enable redist */
+  u32 waker = rr32(cpuid, GICR_WAKER);
+  wr32(cpuid, GICR_WAKER, waker & ~(1<<1));
+  while(rr32(cpuid, GICR_WAKER) & (1<<2))
+    ;
+}
+
 static void gic_enable() {
+  /* enable Group0/Non-secure Group1 */
   wd32(GICD_CTLR, 3);
+
   w_icc_igrpen1_el1(1);
 }
 
-static void gicr_(u32 cpu) {
-  ;
-}
-
 void gicv3_init_percpu() {
+  u32 cpu = cpuid();
+
   gic_cpu_init();
-  gicd_init();
+  gic_dist_init();
+  gic_redist_init(cpu);
 
   gic_enable();
 }
 
 void gicv3_init() {
   gicv3.gicd = (char *)GICBASE;
-  for(int i = 0; i < NCPU; i++)
+  for(int i = 0; i < NCPU; i++) {
     gicv3.rdist_addrs[i] = (char *)(GICBASE + 0xa0000 + (i) * 0x20000);
+    printk("rdist %p ", gicv3.rdist_addrs[i]);
+  }
 
   gic_setup_spi(UART_IRQ, 0);
   gic_setup_spi(VIRTIO_BLK_IRQ, 0);
+  gic_setup_ppi(cpuid(), TIMER_IRQ, 0);
 }
 
 bool gic_enabled() {
